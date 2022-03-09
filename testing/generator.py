28bc27103a4a2708bd02249ebe74c26b5f4e32a9
Sean Shahkarami <sean.shahkarami@gmail.com>
2022-03-09 15:58:04 -0600

    added test node user. adding occasional bad value.

--- testing/generator.py
+++ testing/generator.py
@@ -1,18 +1,26 @@
 import pika
 import time
 import wagglemsg
+from random import uniform
 
 
 credentials = pika.PlainCredentials("node-0000000000000001", "waggle")
 params = pika.ConnectionParameters(credentials=credentials)
 with pika.BlockingConnection(params) as conn:
     ch = conn.channel()
-    # ch.queue_declare("influx-messages", durable=True)
-    msg = wagglemsg.Message(
-        name="env.temperature",
-        value=2.3,
-        timestamp=time.time_ns(),
-        meta={"node": "node-0000000000000001"}
-    )
-    body = wagglemsg.dump(msg)
-    ch.basic_publish("waggle.msg", "", body, pika.BasicProperties(user_id="node-0000000000000001"))
+    while True:
+        if uniform(0.0, 1.0) < 0.9:
+            value = uniform(1.0, 3.0)
+        else:
+            value = "bad"
+
+        msg = wagglemsg.Message(
+            name="env.temperature",
+            value=value,
+            timestamp=time.time_ns(),
+            meta={"node": "0000000000000001"}
+        )
+        body = wagglemsg.dump(msg)
+        properties = pika.BasicProperties(user_id="node-0000000000000001")
+        ch.basic_publish("waggle.msg", "", body, properties)
+        time.sleep(0.1)
